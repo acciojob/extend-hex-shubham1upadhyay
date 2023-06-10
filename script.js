f563133747fa03b60227d9e17dd97803b14beecc
Shubham Upadhyay <shubhamup47@gmail.com>
2023-06-10 12:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const extendHex = (shortHex) => {
-  // write your code here
+	
+	const hex = shortHex.replace("#", "");
+
+	const r = hex[0];
+	const g = hex[1];
+	const b = hex[2];
+	
+	const fullHex = `#${r}${r}${g}${g}${b}${b}`;
+	
+	return fullHex;
 };
 
 // Do not change the code below.
